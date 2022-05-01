e4344365c362f67d43970130efaf6b7ec5a35c55
NicholasAdi24 <102377198+NicholasAdi24@users.noreply.github.com>
2022-05-01 18:20:24 +0700

    Update home.java

--- home.java
+++ home.java
@@ -1,1 +1,283 @@
 
+package tugasmodul8;
+
+public class home extends javax.swing.JFrame {
+
+    public home() {
+        initComponents();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        jPopupMenu2 = new javax.swing.JPopupMenu();
+        jPopupMenu3 = new javax.swing.JPopupMenu();
+        jFrame1 = new javax.swing.JFrame();
+        jLabel8 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jTextFieldusername = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        buttonsubmit = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
+        buttonminimize = new javax.swing.JButton();
+        buttonclose = new javax.swing.JButton();
+        buttonregister = new javax.swing.JButton();
+        passwrong = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gambar/WhatsApp Image 2022-04-28 at 19.12.38.jpeg"))); // NOI18N
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        jPanel1.setLayout(null);
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(0, 0, 360, 0);
+
+        jPanel2.setBackground(new java.awt.Color(72, 61, 139));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Bodoni MT Poster Compressed", 1, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(220, 20, 60));
+        jLabel1.setText("WEEBS");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 180, 120, 50));
+
+        jLabel2.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
+        jLabel2.setText("Password");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 330, 70, -1));
+
+        jLabel3.setFont(new java.awt.Font("Bauhaus 93", 0, 36)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 250, 240));
+        jLabel3.setText("WELCOME !");
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 60, 190, 50));
+
+        jLabel4.setFont(new java.awt.Font("Bauhaus 93", 0, 36)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 250, 240));
+        jLabel4.setText("to");
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 100, 40, 50));
+
+        jLabel5.setFont(new java.awt.Font("Adobe Caslon Pro Bold", 3, 36)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(240, 230, 140));
+        jLabel5.setText("The Adventure of ");
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, 280, 50));
+
+        jLabel6.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
+        jLabel6.setText("Masukkan Username dan password");
+        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 260, 280, -1));
+
+        jLabel7.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
+        jLabel7.setText("Username");
+        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 300, 70, -1));
+
+        jTextFieldusername.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldusernameActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jTextFieldusername, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 300, 190, -1));
+        jPanel2.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 330, 190, -1));
+
+        buttonsubmit.setText("Submit");
+        buttonsubmit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonsubmitActionPerformed(evt);
+            }
+        });
+        jPanel2.add(buttonsubmit, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 390, 80, -1));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 290, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 70, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 290, -1, -1));
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 230, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 20, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 260, -1, -1));
+
+        buttonminimize.setText("_");
+        buttonminimize.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonminimizeActionPerformed(evt);
+            }
+        });
+        jPanel2.add(buttonminimize, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, -1, -1));
+
+        buttonclose.setText("X");
+        buttonclose.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                buttoncloseMouseClicked(evt);
+            }
+        });
+        buttonclose.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttoncloseActionPerformed(evt);
+            }
+        });
+        jPanel2.add(buttonclose, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 10, -1, -1));
+
+        buttonregister.setText("Register");
+        buttonregister.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonregisterActionPerformed(evt);
+            }
+        });
+        jPanel2.add(buttonregister, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 420, 100, -1));
+
+        passwrong.setFont(new java.awt.Font("Swis721 Hv BT", 1, 14)); // NOI18N
+        passwrong.setForeground(new java.awt.Color(255, 0, 51));
+        passwrong.setText("*");
+        jPanel2.add(passwrong, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 370, 280, -1));
+
+        jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Gambar/WhatsApp Image 2022-04-28 at 19.12.38.jpeg"))); // NOI18N
+        jPanel2.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(-340, -40, 700, 540));
+
+        getContentPane().add(jPanel2);
+        jPanel2.setBounds(0, 0, 360, 460);
+
+        pack();
+    }// </editor-fold>                        
+
+    private void jTextFieldusernameActionPerformed(java.awt.event.ActionEvent evt) {                                                   
+        // TODO add your handling code here:
+    }                                                  
+
+    private void buttonsubmitActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        // TODO add your handling code here
+        if (jPasswordField1.getText().equals(register.password)&&(jTextFieldusername.getText().equals(register.name))){
+        Mainmenu mn = new Mainmenu();
+        mn.setVisible(true);
+        mn.pack();
+        mn.setLocationRelativeTo(null);
+        mn.setDefaultCloseOperation(home.EXIT_ON_CLOSE);}
+        else {
+            passwrong.setText("Masukan password salah");
+        }
+    }                                            
+
+    private void buttoncloseActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+        System.exit(0);
+    }                                           
+
+    private void buttonregisterActionPerformed(java.awt.event.ActionEvent evt) {                                               
+        // TODO add your handling code here:
+        register rg = new register();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(register.EXIT_ON_CLOSE);
+    }                                              
+ 
+    private void buttoncloseMouseClicked(java.awt.event.MouseEvent evt) {                                         
+        // TODO add your handling code here:
+        System.exit(0);
+    }                                        
+
+    private void buttonminimizeActionPerformed(java.awt.event.ActionEvent evt) {                                               
+        // TODO add your handling code here:
+        this.setState(1);
+    }                                              
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(home.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new home().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton buttonclose;
+    private javax.swing.JButton buttonminimize;
+    private javax.swing.JButton buttonregister;
+    private javax.swing.JButton buttonsubmit;
+    private javax.swing.JFrame jFrame1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JPopupMenu jPopupMenu1;
+    private javax.swing.JPopupMenu jPopupMenu2;
+    private javax.swing.JPopupMenu jPopupMenu3;
+    private javax.swing.JTextField jTextFieldusername;
+    private javax.swing.JLabel passwrong;
+    // End of variables declaration                   
+}
+
